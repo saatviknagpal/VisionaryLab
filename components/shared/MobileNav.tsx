3a356d9793eb781c615d3196694e758a098ba3ef
Saatvik <nagpal.saa@northeastern.edu>
2024-05-18 21:09:19 -0700

    added react-form-hooks & header components with integration of cloudinary AI

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,65 +1,84 @@
 "use client"
 
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { navLinks } from "@/constants"
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Button } from "../ui/button"
-  
+
 const MobileNav = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
+
   return (
     <header className="header">
-        <Link href="/" className="flex items-center gap-2 md:py-2">
-            <Image src="/assets/images/logo-text.png" alt="logo" width={55} height={30} className="m-auto" />
-        </Link>
-        <nav className="flex gap-2">
-            <SignedIn>
-                <UserButton afterSignOutUrl="/" showName />
-                <Sheet>
-  <SheetTrigger><Image src="/assets/icons/menu.svg" alt="menu" width={32} height={32} className="cursor-pointer"/></SheetTrigger>
-  <SheetContent className="sheet-content sm:w-64">
-    <>
-        <Image src="/assets/images/logo-text.png" alt="logo" width={100} height={28} className="m-auto" />
-        <ul className='sidebar-nav_elements'>
-                    {navLinks.map((link) => {
-                        const isActive = link.route === pathname
+      <Link href="/" className="flex items-center gap-2 md:py-2">
+        <Image
+          src="/assets/images/logo-text.svg"
+          alt="logo"
+          width={55}
+          height={30}
+        />
+      </Link>
 
-                        return (
-                            <li key = {link.route} 
-                            className={`${isActive && 'gradient-text'} p-18 flex whitespace-nowrap text-dark-700`}>
-                                <Link className='sidebar-link cursor-pointer' href={link.route}>
-                                    <Image src = {link.icon} alt="logo" width={24} height={24}/>
-                                        {link.label}
-                                </Link>
-                            </li>
-                    )})}
-                    <li className='flex-center cursor-pointer gap-2 p-4'>
-                        <UserButton afterSignOutUrl='/' showName />
-                    </li>
-                    </ul>
-    </>
-  </SheetContent>
-</Sheet>
+      <nav className="flex gap-2">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
 
-            </SignedIn>
+          <Sheet>
+            <SheetTrigger>
+              <Image 
+                src="/assets/icons/menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="cursor-pointer"
+              />
+            </SheetTrigger>
+            <SheetContent className="sheet-content sm:w-64">
+              <>
+                <Image 
+                  src="/assets/images/logo-text.svg"
+                  alt="logo"
+                  width={100}
+                  className="mx-auto"
+                  height={28}
+                />
 
-            <SignedOut>
+              <ul className="header-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname
 
-            <Button asChild className='button bg-purple-gradient bg-cover'>
-                        <Link href="/sign-in">Login</Link>
-                    </Button>
-            </SignedOut>
-        </nav>
+                return (
+                  <li 
+                    className={`${isActive && 'gradient-text'} p-18 flex whitespace-nowrap text-dark-700`}
+                    key={link.route}
+                    >
+                    <Link className="sidebar-link cursor-pointer" href={link.route}>
+                      <Image 
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                )
+              })}
+              </ul>
+              </>
+            </SheetContent>
+          </Sheet>
+        </SignedIn>
+
+        <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+      </nav>
     </header>
   )
 }
---
+++ components/shared/TransformedImage.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+
+const TransformedImage = ({image, type, title, transformationConfig, isTransforming, setIsTransforming, hasDownload=false}: TransformedImageProps) => {
+
+    const downloadHandler = () => {}
+  return (
+    <div className='flex flex-col gap-4'>
+
+        <div className='flex-between'>
+            <h3 className='h3-bold text-dark-600'> 
+            Transformed
+            </h3>
+
+            {hasDownload && (
+                <button className='download-btn' onClick={downloadHandler}>
+                    <Image src="/assets/icons/download.svg" alt="download" width={24} height={24} className='pb-[6px]'/>
+                </button>
+            )}
+        </div>
+    </div>
+  )
+}
+
+export default TransformedImage
